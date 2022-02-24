a7f9b0284ef7f815b81cd36eb16058c94f6c8443
sebastianzuluagac <sebastian.zuluagac@udea.edu.co>
2022-02-23 23:05:18 -0500

    Creacion_Rama_punto_evaluativo_2

---
+++ Punto_evaluativo_2_Taller/main.cpp
@@ -0,0 +1,23 @@
+//Enlace a simulacion en Tinkercad.
+//https://www.tinkercad.com/things/lhkbmXPq2k1-comunicacion2arduinos/editel
+
+#include <iostream>
+void Arduino_1();
+void Arduino_2();
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+void Arduino_1(){
+
+}
+
+void Arduino_2(){
+
+}
